f55b5ba15e027d511a82f3513b9db49144d546d1
Jorge Fabrizio Lopez Quiroz <giorgiofabrilopez@gmail.com>
2021-04-07 00:34:30 +0200

    Modifiche e bonus

--- js/main.js
+++ js/main.js
@@ -1,16 +1,55 @@
 var app = new Vue({
     el: "#app",
     data: {
-        mySongs : []
+        songs : [],
+        genres: ['All'],
+        selected: 'All'
     },
-    mounted() {  
+    mounted() {
         axios
-        .get('https://flynn.boolean.careers/exercises/api/array/music')
-        .then(result => {
-            result.data.response.forEach(element => {
-                this.mySongs.push(element);
+            .get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then((result) => {
+                this.songs = result.data.response
+                this.songs.forEach(song => {
+                    if(!this.genres.includes(song.genre)) {
+                        this.genres.push(song.genre);
+                    }
+                });
             });
-        });
+    },
+    methods: {
+        show(song) {
+            if (song.genre == this.selected || this.selected == 'All') {
+                return true;
+            }
+            else {
+                return false;
+            }
+        },
+        ascendingOrder() {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then((result) => {
+                this.songs = result.data.response.sort((a,b) => a.year - b.year);
+                this.songs.forEach(song => {
+                    if(!this.genres.includes(song.genre)) {
+                        this.genres.push(song.genre);
+                    }
+                });
+            });
+        },
+        descendingOrder() {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then((result) => {
+                this.songs = result.data.response.sort((a,b) => b.year - a.year);
+                this.songs.forEach(song => {
+                    if(!this.genres.includes(song.genre)) {
+                        this.genres.push(song.genre);
+                    }
+                });
+            });
+        }
     }
 });
 
